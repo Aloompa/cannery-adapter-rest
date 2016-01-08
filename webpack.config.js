63928975fdcd2ccd763c072a90399c6d98aac8dd
Tyson Cadenhead <tysoncadenhead@tysons-mbp-2.attlocal.net>
2016-01-08 12:34:52 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+    	'cannery-adapter-rest': './src'
+    },
+    output: {
+    	path: path.resolve(__dirname) + '/dist',
+    	filename: '[name].js',
+    	sourceMapFilename: '[name].map'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: /src/,
+            query: {
+                presets: ['es2015', 'stage-0']
+            }
+        }]
+    },
+    plugings: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+};
